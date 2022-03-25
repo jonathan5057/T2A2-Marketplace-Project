693990184ec328e86950f497df8d63e5d365d371
jonathan5057 <GCAB022114@coderacademy.edu.au>
2022-03-26 00:09:46 +1000

    added order to route

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_one :warehouse
   accepts_nested_attributes_for :warehouse
+  has_many :orders
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "listings#index"
-  resources :listings
+  resources :listings do 
+    resources :orders, only: [:new, :create]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
